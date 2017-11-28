4616ac4b7dbad75168f6021de35172ee1640f1e9
github username <github email address>
2017-11-28 14:32:53 -0500

    added routes for players

--- nba-team-search-api/config/routes.rb
+++ nba-team-search-api/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'home#index'
+
   namespace :api do
-    resources :teams, except: [:new, :edit]
-    resources :users, only: [:create]
+    resources :players, :only => [:index, :create, :show, :update]
+    resources :teams, except: [:new, :edit] do
+      resources :players, :only =>[:index]
+    end
   end
 end

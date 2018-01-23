dc827c4aba0a52f84568fdfb6a2d50c4ff8a751f
github username <github email address>
2018-01-23 17:51:16 -0500

    changed to https

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ const Home = (props) => {
      </div>
       <div className="padding-top">
         <p className="padding-top">Manage your teams and players all in one app</p>
-        <p> This is an open sourced project, so if you would like to contribute please go to my        <a href="https://github.com/samsonyuwono/nba-team-search" target="_blank" rel="noopener noreferrer"> Github Page</a></p>
+        <p> This is an open sourced project, so if you would like to contribute please go to my        <a href="https://github.com/samsonyuwono/rec-league-app" target="_blank" rel="noopener noreferrer"> Github Page</a></p>
         <p>Created by <a href="https://github.com/samsonyuwono/" target="_blank" rel="noopener noreferrer">Samson Yuwono</a> as a project at the Flatiron School</p></div>
       </div>
   )
